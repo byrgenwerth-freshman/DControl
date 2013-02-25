97ddf91ae7f4997eb0f747bbbf8262d3ffc173cf
Matt Owens <maog38@mail.umkc.edu>
2013-02-25 18:35:35 -0500

    Made a mobile css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def mobile_device?
+    request.user_agent =~ /Mobile/
+  end
+  helper_method :mobile_device?
 end
